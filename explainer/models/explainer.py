e338f5b135d54f771504e6839c7be2528caecbe0
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-28 17:42:45 +0700

    test

--- explainer/models/explainer.py
+++ explainer/models/explainer.py
@@ -187,7 +187,7 @@ class Explainer(torch.nn.Module):
         )
 
         # Convert tokenized input IDs to model's embeddings
-        inputs_embeds = self.model.get_input_embeddings()(tokenized_inputs['input_ids'])
+        inputs_embeds = self.model.get_input_embeddings()(tokenized_inputs['input_ids'].to(self.device))
         
         # Get the token ID for the <USER_EMBED> <ITEM_EMBED> token
         user_embed_token_id = self.tokenizer.convert_tokens_to_ids("<USER_EMBED>")
