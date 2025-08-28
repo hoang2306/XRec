095c7dd5c8a2250609f80f3d58be74261e449d17
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-28 18:07:32 +0700

    test

--- explainer/models/modeling_explainer.py
+++ explainer/models/modeling_explainer.py
@@ -1130,6 +1130,7 @@ class LlamaModel(LlamaPreTrainedModel):
         causal_mask = torch.full((sequence_length, target_length), fill_value=min_dtype, dtype=dtype, device=device)
         if sequence_length != 1:
             causal_mask = torch.triu(causal_mask, diagonal=1)
+        print(f'causal_mask shape: {causal_mask.shape}')
         causal_mask *= torch.arange(target_length, device=device) > cache_position.reshape(-1, 1)
         causal_mask = causal_mask[None, None, :, :].expand(input_tensor.shape[0], 1, -1, -1)
         if attention_mask is not None:
