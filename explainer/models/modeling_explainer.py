a509934758fce9a01f92212a7f9b769b898fe524
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-28 18:13:19 +0700

    test

--- explainer/models/modeling_explainer.py
+++ explainer/models/modeling_explainer.py
@@ -1112,6 +1112,9 @@ class LlamaModel(LlamaPreTrainedModel):
     # (`recording cudagraph tree for symint key 13`, etc.), which is VERY slow. A workaround is `@torch.compiler.disable`, but this prevents using
     # `fullgraph=True`. See more context in https://github.com/huggingface/transformers/pull/29114
     def _update_causal_mask(self, attention_mask, input_tensor, cache_position):
+
+        print(f'input_tensor shape: {input_tensor.shape}')
+
         if self.config._attn_implementation == "flash_attention_2":
             if attention_mask is not None and 0.0 in attention_mask:
                 return attention_mask
