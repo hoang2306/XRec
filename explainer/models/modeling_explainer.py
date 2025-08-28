ff9933a959186e5164fff0e907eae3415c7fa0fc
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-28 17:01:33 +0700

    test

--- explainer/models/modeling_explainer.py
+++ explainer/models/modeling_explainer.py
@@ -1025,7 +1025,11 @@ class LlamaModel(LlamaPreTrainedModel):
         past_seen_tokens = 0
         if use_cache:  # kept for BC (cache positions)
             if not isinstance(past_key_values, StaticCache):
-                past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+                # past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+                if past_key_values is None or all(x is None for x in past_key_values):
+                    past_key_values = None  # Không dùng cache
+                else:
+                    past_key_values = DynamicCache.from_legacy_cache(past_key_values)
                 past_seen_tokens = past_key_values.get_seq_length()
 
         if cache_position is None:
