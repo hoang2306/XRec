888cec152ef5fea5afa449b66a5c5ab512d5fd3c
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-28 18:23:38 +0700

    test

--- explainer/models/modeling_explainer.py
+++ explainer/models/modeling_explainer.py
@@ -1261,6 +1261,9 @@ class LlamaForCausalLM(LlamaPreTrainedModel, GenerationMixin):
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+
+        print(f'input_ids in LlamaModel shape: {input_ids.shape}')
+
         outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
