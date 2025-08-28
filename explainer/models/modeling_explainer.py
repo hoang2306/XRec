b427ccef2ccc387015c72564a91d457fa6da2b4c
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-28 18:24:55 +0700

    test

--- explainer/models/modeling_explainer.py
+++ explainer/models/modeling_explainer.py
@@ -1254,6 +1254,8 @@ class LlamaForCausalLM(LlamaPreTrainedModel, GenerationMixin):
         >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
         ```"""
+
+        print(f'input_ids in LlamaModel shape: {input_ids.shape}')
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
